ff7b1e0ea78ca33889293e3a845ef0ebe913962b
jbratcher <jeremybratcher@gmail.com>
2019-12-02 07:50:19 -0500

    linting config, added netlify identitiy widget

--- pages/index.vue
+++ pages/index.vue
@@ -87,6 +87,12 @@ export default {
   components: {
     Logo,
     VuetifyLogo
-  }
-}
+  },
+  head () {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+};
+
 </script>
